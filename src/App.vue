d38a6f3d3c2eb873164f57711634b4fa1c0d8d0e
roma_marshall <roma_marshall@outlook.com>
2022-04-05 01:57:19 +0200

    ul li edit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <script setup>
 const menu = ['buy crypto', 'markets', 'trade', 'finance', 'learn', 'support', 'more']
 const target = ['Popular assets', 'New assets', 'Gains ranking', 'Turnover ranking']
+const fieldName = ['Asset', 'Last price', 'Today\'s change', 'Chart', 'Trade']
+const crypto = ['BTC', '46000$', '+0.35%', 'chart', 'trade']
+const crypto2 = ['ETH', '4300$', '+0.25%', 'chart', 'trade']
 </script>
 
 <template>
@@ -13,7 +16,8 @@ const target = ['Popular assets', 'New assets', 'Gains ranking', 'Turnover ranki
               <svg version="1.2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 161 48" width="161" height="48">
                 <path fill="#fff" d="m42.3 43.2v-2l86.7 0.3v2z"/>
                 <text style="transform: matrix(1,0,0,1,42,36)">
-                  <tspan x="0" y="0" font-size="24px" fill="#ffffff" font-weight="400" font-family="DejaVuSans">E</tspan>
+                  <tspan x="0" y="0" font-size="24px" fill="#ffffff" font-weight="400" font-family="DejaVuSans">E
+                  </tspan>
                   <tspan y="0" font-size="24px" fill="#ffffff" font-weight="400" font-family="DejaVuSans">X</tspan>
                   <tspan y="0" font-size="24px" fill="#ffffff" font-weight="400" font-family="DejaVuSans">c</tspan>
                   <tspan y="0" font-size="24px" fill="#ffffff" font-weight="400" font-family="DejaVuSans">h</tspan>
@@ -47,6 +51,18 @@ const target = ['Popular assets', 'New assets', 'Gains ranking', 'Turnover ranki
               {{ item }}
             </a>
           </div>
+          <div class="flex justify-center items-center text-xs text-gray-400 space-x-10 py-10">
+            <ul v-for="item, index in fieldName">
+              {{ item }}
+              <li>
+                {{ crypto[index] }}
+              </li>
+              <li>
+                {{ crypto2[index] }}
+              </li>
+            </ul>
+          </div>
+
         </div>
       </div>
     </div>
