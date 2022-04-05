212cd9513759a063263ca397d14f190de34446e6
roma_marshall <roma_marshall@outlook.com>
2022-04-06 01:25:19 +0200

    vite-svg-loader install & logo svg edit

--- src/App.vue
+++ src/App.vue
@@ -5,21 +5,7 @@
         <div class="px-6 mx-auto max-w-6xl">
           <div class="flex justify-between items-center py-10">
             <div class="cursor-pointer">
-              <svg version="1.2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 161 48" width="161" height="48">
-                <path fill="#fff" d="m42.3 43.2v-2l86.7 0.3v2z"/>
-                <text style="transform: matrix(1,0,0,1,42,36)">
-                  <tspan x="0" y="0" font-size="24px" fill="#ffffff" font-weight="400" font-family="DejaVuSans">E</tspan>
-                  <tspan y="0" font-size="24px" fill="#ffffff" font-weight="400" font-family="DejaVuSans">X</tspan>
-                  <tspan y="0" font-size="24px" fill="#ffffff" font-weight="400" font-family="DejaVuSans">c</tspan>
-                  <tspan y="0" font-size="24px" fill="#ffffff" font-weight="400" font-family="DejaVuSans">h</tspan>
-                  <tspan y="0" font-size="24px" fill="#ffffff" font-weight="400" font-family="DejaVuSans">a</tspan>
-                  <tspan y="0" font-size="24px" fill="#ffffff" font-weight="400" font-family="DejaVuSans">n</tspan>
-                  <tspan y="0" font-size="24px" fill="#ffffff" font-weight="400" font-family="DejaVuSans">g</tspan>
-                  <tspan y="0" font-size="24px" fill="#ffffff" font-weight="400" font-family="DejaVuSans">e</tspan>
-                </text>
-                <path fill="none" stroke="#ffffff" stroke-width="3" d="m2 17h25v25h-25z"/>
-                <path fill="none" stroke="#ffffff" stroke-width="3" d="m14 5h25v25h-25z"/>
-              </svg>
+              <Logo />
             </div>
             <nav class="flex items-center space-x-10">
               <a
@@ -65,8 +51,12 @@
 
 <script>
 import {ref} from 'vue'
+import Logo from './assets/img/logo.svg'
 
 export default {
+  components: {
+    Logo
+  },
   setup() {
     const menu = ['buy crypto', 'markets', 'trade', 'finance', 'learn', 'support', 'more']
     const target = ['Popular assets', 'New assets', 'Gains ranking', 'Turnover ranking']
@@ -81,6 +71,7 @@ export default {
         .then(data => result.value = data)
 
     return {
+      Logo,
       menu,
       target,
       fieldName,
